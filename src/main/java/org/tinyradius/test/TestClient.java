376e8818f8aa49a9dd26d6bb2bc9a96eb2321e28
lm <valar_lm@qq.com>
2022-08-26 23:57:27 +0800

    update pom

--- src/main/java/org/tinyradius/test/TestClient.java
+++ src/main/java/org/tinyradius/test/TestClient.java
@@ -25,26 +25,21 @@ public class TestClient {
 	 */
 	public static void main(String[] args) 
 	throws Exception {
-		if (args.length != 4) {
-			System.out.println("Usage: TestClient hostName sharedSecret userName password");
-			System.exit(1);
-		}
-		
-		String host = args[0];
-		String shared = args[1];
-		String user = args[2];
-		String pass = args[3];
+		String host = "192.168.54.66";
+		String shared = "key";
+		String user = "test1";
+		String pass = "password1";
 		
 		RadiusClient rc = new RadiusClient(host, shared);
 
 		// 1. Send Access-Request
 		AccessRequest ar = new AccessRequest(user, pass);
 		ar.setAuthProtocol(AccessRequest.AUTH_PAP); // or AUTH_CHAP
-		ar.addAttribute("NAS-Identifier", "this.is.my.nas-identifier.de");
-		ar.addAttribute("NAS-IP-Address", "192.168.0.100");
-		ar.addAttribute("Service-Type", "Login-User");
-		ar.addAttribute("WISPr-Redirection-URL", "http://www.sourceforge.net/");
-		ar.addAttribute("WISPr-Location-ID", "net.sourceforge.ap1");
+//		ar.addAttribute("NAS-Identifier", "this.is.my.nas-identifier.de");
+		ar.addAttribute("NAS-IP-Address", "192.168.54.66");
+//		ar.addAttribute("Service-Type", "Login-User");
+		/*ar.addAttribute("WISPr-Redirection-URL", "http://www.sourceforge.net/");
+		ar.addAttribute("WISPr-Location-ID", "net.sourceforge.ap1");*/
 		
 		System.out.println("Packet before it is sent\n" + ar + "\n");
 		RadiusPacket response = rc.authenticate(ar);
@@ -52,14 +47,14 @@ public class TestClient {
 		System.out.println("Response\n" + response + "\n");
 
 		// 2. Send Accounting-Request
-		AccountingRequest acc = new AccountingRequest("mw", AccountingRequest.ACCT_STATUS_TYPE_START);
+		/*AccountingRequest acc = new AccountingRequest("mw", AccountingRequest.ACCT_STATUS_TYPE_START);
 		acc.addAttribute("Acct-Session-Id", "1234567890");
 		acc.addAttribute("NAS-Identifier", "this.is.my.nas-identifier.de");
 		acc.addAttribute("NAS-Port", "0");
 	
 		System.out.println(acc + "\n");	
 		response = rc.account(acc);
-		System.out.println("Response: " + response);
+		System.out.println("Response: " + response);*/
 		
 		rc.close();
 	}
